f5966783dfbe1cc40f362ac63d02658e260f656c
Hyliard <luisgerardomartinezh@gmail.com>
2024-05-08 14:55:50 -0300

    Initial Commit

---
+++ curso udemy/curso_udemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  curso_udemyApp.swift
+//  curso udemy
+//
+//  Created by Luis Martinez on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct curso_udemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
